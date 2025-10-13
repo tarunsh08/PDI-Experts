7152531556b212953695533643c4e1e5c9d33af6
Tarun Sharma <tarunsharma08.com@gmail.com>
2025-10-14 00:41:12 +0530

    updated frontend url

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://pdi-experts-l99p.vercel.app",
     credentials: true
 }));
 
